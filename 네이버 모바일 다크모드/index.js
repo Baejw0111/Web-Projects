341de0130d83dbff2ca101ba405914034170f20a
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-04-10 08:47:56 +0900

    네이버 다크모드

--- 네이버 모바일 다크모드/index.js
+++ 네이버 모바일 다크모드/index.js
@@ -7,6 +7,10 @@ const bodyBackground = document.querySelector('body');
 // header-nav
 const headerNav = document.querySelector('.header-nav-list-wrapper')
 
+const menu=document.querySelector('.header-nav-menu')
+const pay=document.querySelector('.header-nav-pay')
+const notice=document.querySelector('.header-nav-notice')
+
 // bookmark-wrapper
 const bookmarkWrapper = document.querySelector('.bookmark-wrapper')
 
@@ -19,6 +23,8 @@ const imgIconWrappers = document.querySelectorAll('.img-icon-wrapper')
 // bookmark-text
 const bookmarkTexts = document.querySelectorAll('.bookmark-text')
 
+const searchWindow = document.querySelector('.search-window')
+
 toggleButton.addEventListener('click', function () {
     // 클릭
     // 다크모드로 전환되는 상태
@@ -29,6 +35,11 @@ toggleButton.addEventListener('click', function () {
     toggleButton.classList.toggle('toggle-button-darkmode')
     bodyBackground.classList.toggle('body-background-darkmode')
     headerNav.classList.toggle("text-darkmode")
+    searchInput.classList.toggle("search-input-darkmode")
+
+    menu.src="./images/메뉴.png"
+    pay.src="./images/pay.png"
+    notice.src="./images/알림.png"
 
     for (let i = 0; i < imgIconWrappers.length; ++i) {
         imgIconWrappers[i].classList.toggle('img-icon-wrapper-darkmode')
@@ -38,9 +49,14 @@ toggleButton.addEventListener('click', function () {
         bookmarkTexts[i].classList.toggle('text-darkmode')
     }
 
+    searchWindow.classList.toggle('search-window-darkmode')
+
     // classList.contains 활용
     if (toggleButton.classList.contains('toggle-button-darkmode')) {
         toggleButton.textContent = "일반 모드"
+        menu.src="./images/메뉴_negative.png"
+        pay.src="./images/pay_negative.png"
+        notice.src="./images/알림_negative.png"
     }
 })
 
